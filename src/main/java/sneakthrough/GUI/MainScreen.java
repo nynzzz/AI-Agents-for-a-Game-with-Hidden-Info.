c34beecb3dadfd892e187f8002746b9e0b16b593
Gabriel <eutu20032003@gmail.com>
2023-09-20 14:35:29 +0200

    main--

--- src/main/java/sneakthrough/GUI/MainScreen.java
+++ src/main/java/sneakthrough/GUI/MainScreen.java
@@ -1,6 +1,7 @@
 package sneakthrough.GUI;
 
 import javafx.application.Application;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
@@ -12,13 +13,15 @@ import javafx.scene.shape.Rectangle;
 import javafx.scene.text.Font;
 import javafx.scene.text.FontWeight;
 import javafx.scene.text.TextAlignment;
+import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 public class MainScreen extends Application {
 
-    final int ScreenWIDTH = 1920;
-    final int ScreenHEIGHT = 1080;
+    Rectangle2D primaryScreenBounds = Screen.getPrimary().getVisualBounds();
 
+    final double screenWidth = primaryScreenBounds.getWidth();
+    final double screenHeight = primaryScreenBounds.getHeight();
     final int boardSize = 8;
 
     @Override
@@ -26,29 +29,32 @@ public class MainScreen extends Application {
 
         stage.setTitle("Sneakthrough game - Group 6");
 
+
+
         Group group = new Group();
 
-        Scene mainMenu = new Scene(group, ScreenWIDTH, ScreenHEIGHT, true);
+        Scene mainMenu = new Scene(group, screenWidth, screenHeight, true);
         mainMenu.setFill(Color.LIGHTBLUE);
+        mainMenu.setRoot(group);
 
         //single player
         Group singleGroup = new Group();
-        Scene singlePlayerScene = new Scene(singleGroup, ScreenWIDTH, ScreenHEIGHT);
+        Scene singlePlayerScene = new Scene(singleGroup, screenWidth, screenHeight);
         singlePlayerScene.setFill(Color.BLACK);
 
         //multiplayer
         Group multiplayerGroup = new Group();
-        Scene multiPLayerScene = new Scene(multiplayerGroup, ScreenWIDTH, ScreenHEIGHT);
+        Scene multiPLayerScene = new Scene(multiplayerGroup, screenWidth, screenHeight);
         multiPLayerScene.setFill(Color.LIGHTSEAGREEN);
 
         GridPane chessboard = new GridPane();
-        chessboard.setLayoutX(570);
-        chessboard.setLayoutY(150);
+        chessboard.setLayoutX(screenWidth/2 - 280);
+        chessboard.setLayoutY(screenHeight/2 -280);
 
         for (int row = 0; row < boardSize; row++)
         {
             for (int col = 0; col < boardSize; col++) {
-                Rectangle square = new Rectangle(100, 100);
+                Rectangle square = new Rectangle(70, 70);
                 if ((row + col) % 2 == 0) {
                     square.setFill(Color.WHITE);
                 } else {
@@ -58,15 +64,15 @@ public class MainScreen extends Application {
 
                 if (row == 1 || row == 0)
                 {
-                    Circle pawn = new Circle(30, Color.BROWN);
-                    pawn.setTranslateX(20);
+                    Circle pawn = new Circle(20, Color.BROWN);
+                    pawn.setTranslateX(15);
                     chessboard.add(pawn, col , row);
                 }
 
                 if (row == 6 || row == 7)
                 {
-                    Circle pawn = new Circle(30, Color.SANDYBROWN);
-                    pawn.setTranslateX(20);
+                    Circle pawn = new Circle(20, Color.SANDYBROWN);
+                    pawn.setTranslateX(15);
                     chessboard.add(pawn, col , row);
                 }
             }
@@ -74,32 +80,34 @@ public class MainScreen extends Application {
 
 
         // labels for main menu
-        Label gameName = new Label("Sneakthrough") ;
+        Label gameName = new Label("Sneakthrough");
+        //gameName.setPrefSize(300, 100);
+        gameName.setTextAlignment(TextAlignment.CENTER);
+
         Label groupNumber = new Label("Made by Group 6");
+       // groupNumber.setPrefSize(300, 80); // Changed from gameName.setPrefSize
+        groupNumber.setTextAlignment(TextAlignment.CENTER);
 
         // Define the Font here
         Font font = Font.font("Helvetica Neue", FontWeight.NORMAL, 30);
         gameName.setFont(font);
         groupNumber.setFont(font);
 
-        gameName.setTextAlignment(TextAlignment.RIGHT);
-        groupNumber.setTextAlignment(TextAlignment.RIGHT);
-
         gameName.setTextFill(Color.BLACK);
         groupNumber.setTextFill(Color.BLACK);
 
-        gameName.setLayoutX(880);
-        gameName.setLayoutY(200);
+        gameName.setLayoutX(screenWidth / 2 -100);
+        gameName.setLayoutY(screenHeight/6);
 
-        groupNumber.setLayoutX(860);
-        groupNumber.setLayoutY(240);
+        groupNumber.setLayoutX(screenWidth / 2 - 100);
+        groupNumber.setLayoutY(screenHeight/6 + 100);
 
         //buttons for main menu
 
         Button multiPlayer = new Button("Multiplayer - Human vs Human");
         multiPlayer.setPrefWidth(200);
-        multiPlayer.setLayoutX(880);
-        multiPlayer.setLayoutY(390);
+        multiPlayer.setLayoutX(screenWidth/2 - 250);
+        multiPlayer.setLayoutY(screenHeight/2);
         multiPlayer.setOnAction(e ->
         {
             stage.setScene(multiPLayerScene);
@@ -107,8 +115,8 @@ public class MainScreen extends Application {
 
         Button singlePlayer = new Button("Single player");
         singlePlayer.setPrefWidth(200);
-        singlePlayer.setLayoutX(880);
-        singlePlayer.setLayoutY(340);
+        singlePlayer.setLayoutX(screenWidth/2 + 50);
+        singlePlayer.setLayoutY(screenHeight/2);
         singlePlayer.setOnAction(e ->
         {
             stage.setScene(singlePlayerScene);
@@ -120,6 +128,8 @@ public class MainScreen extends Application {
         stage.setScene(mainMenu);
         stage.show();
         stage.setMaximized(true);
+        System.out.println(gameName.getPrefWidth());
+
     }
 
 }
